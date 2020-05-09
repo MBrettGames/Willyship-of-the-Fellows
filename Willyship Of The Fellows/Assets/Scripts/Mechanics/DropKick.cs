c25191582df1b8f42db8f4a76a83a2d36bc77c80
MattBrettGameArt <47274870+MattBrettGameArt@users.noreply.github.com>
2020-05-09 22:46:24 +0100

    drop kick stuff

---
+++ Willyship Of The Fellows/Assets/Scripts/Mechanics/DropKick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropKick : MonoBehaviour
+{
+    private NPCRagdoll npcRagdoll;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        npcRagdoll = other.GetComponent<NPCRagdoll>();
+        if (other.CompareTag("HurtableNPC"))
+        {
+            npcRagdoll.TurnOnRagdoll();
+        }
+    }
+}
---
+++ Willyship Of The Fellows/Assets/Scripts/Mechanics/NPCRagdoll.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCRagdoll : MonoBehaviour
+{
+
+    private bool b_isRagdolling;
+    private Rigidbody rb;
+    Animator anim;
+
+    public List<Collider> RagdollParts = new List<Collider>();
+
+
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Awake()
+    {
+        SetRagdollParts();
+        SetPlayerCollider();
+       // TurnOffRagdoll();
+    }
+
+
+    private void SetRagdollParts()
+    {
+        Collider[] colliders = this.gameObject.GetComponentsInChildren<Collider>();
+
+        foreach (Collider col in colliders)
+        {
+            if (col.CompareTag("RagdollPart") && col.gameObject != this.gameObject)
+            {
+                col.isTrigger = true;
+                RagdollParts.Add(col);
+            }
+        }
+    }
+
+    private void SetPlayerCollider()
+    {
+        CapsuleCollider capsule = GetComponent<CapsuleCollider>();
+        Debug.Log("I'm a player collider");
+
+    }
+
+    public void TurnOnRagdoll()
+    {
+        rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+        this.gameObject.GetComponent<CapsuleCollider>().enabled = false;
+        anim.enabled = false;
+        b_isRagdolling = true;
+        Debug.Log("I'm ragdolling");
+
+        foreach (Collider col in RagdollParts)
+        {
+            col.isTrigger = false;
+            col.attachedRigidbody.velocity = Vector3.zero;
+        }
+    }
+
+    public void TurnOffRagdoll()
+    {
+        rb.useGravity = true;
+        this.gameObject.GetComponent<CapsuleCollider>().enabled = true;
+        anim.enabled = true;
+        b_isRagdolling = false;
+        Debug.Log("I'm not ragdolling");
+
+        foreach (Collider col in RagdollParts)
+        {
+            col.isTrigger = true;
+        }
+    }
+
+}
