b1c4d4136d85f74deb33e73d6914d74190488c1d
MattBrettGameArt <47274870+MattBrettGameArt@users.noreply.github.com>
2020-06-10 02:31:44 +0100

    xxxtentacion is back

---
+++ Willyship Of The Fellows/Assets/Scripts/Performance/HideOnExit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideOnExit : MonoBehaviour
+{
+    [SerializeField] GameObject GO_Parent;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GO_Parent.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GO_Parent.SetActive(false);
+        }
+    }
+}
